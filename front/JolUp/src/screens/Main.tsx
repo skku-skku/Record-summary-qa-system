3865c8441c75a123fbb148b32f327e764f50ec73
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-03-26 01:15:27 +0900

    [feat] add pages

--- front/JolUp/src/screens/Main.tsx
+++ front/JolUp/src/screens/Main.tsx
@@ -15,9 +15,8 @@ export default function MainScreen() {
   const [value, onChange] = useState(new Date());
     return (
       <Container1>
-        {/* <Calendar onChange={onChange} value={value} /> */}
         <Container3>
-
+            {/* <Calendar onChange={onChange} value={value} /> */}
         </Container3>
         <Container2>
             <MainCard title={"클라이언트 1차 미팅-1"} date={"2023년 3월 25일 토요일"} context={"주요안건은 다음과 같습니다.  상품출고일까지 기한을 지켜서 준비를 완료시켜야 합니다. 상품에 마감 처리에 좀 더 신경을 써야하며, 색상도 알맞게 나왔는지 확인하는 것이 중요합니다. 가장 중요한 것은.."}/>
