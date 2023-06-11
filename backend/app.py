84cb96612ef304574c2b6a8d8e56c08bd2014f4e
KaeHyun <80453200+KaeHyun@users.noreply.github.com>
2023-06-11 18:44:19 +0900

    [REFACTOR] api_key 숨기기

--- backend/app.py
+++ backend/app.py
@@ -10,11 +10,14 @@ from pymongo import MongoClient
 from load_model import LoadModel
 import speech_recognition as sr
 import openai
-import datetime
+from dotenv import load_dotenv
+import os
 
-# API key 업데이트
-openai.api_key = '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'
 
+load_dotenv()
+
+# API key 가져오기
+openai.api_key = os.environ.get('API_KEY')
 
 app = Flask(__name__)
 CORS(app)
@@ -93,20 +96,20 @@ class UpLoader(Resource):
                 offset += duration
     
     #요약 실행
-    tmp= "긴급 회의인데 모두 모여주셔서 감사합니다. 오늘 교수님과 면담한 내용을 바탕으로 저희 프로젝트의 세부사항에대해서 이야기를 해봐야될 것 같아요. 네, 안그래도 제가 생각한 부분은 유저가 어플을 사용할 때 시나리오 흐름의 구성이 부족한 것 같아요. 우선, 그 부분을 먼저 이야기 할까요? 좋습니다. 음, 사용자가 어플을 키면 바로 카메라가 켜지고 경계선이 존재해야할 것 같아요. 경계선이요? 네, 시각 장애인 분들을위한 프로젝트니까 화면에 정확하게 보일러를 찍으시긴 어려울테니, 경계를 만들고 가이드라인을 제시하는거죠. 아 무슨 말인지 알겠어요. 초기에 나왔던 아이디어였던 것 같은데 이 부분은 다시 추가하는게 좋겠네요. 아! 그리고 음성 인식을 진행해서 사용자의 요구사항을 이해하는 방향은 어떤가요? 보일러 사진에대해서 사용 설명서처럼 읊어주면 오늘 들은대로 너무 간단한 기술이될 것 같아요. 듣고보니 그렇네요? 그럼 이 부분은 노션에 추가할테니 다음 정기회의 때 더 이야기해보는게 좋겠어요. 그리고 다음 이야기 나누어봐야할 것이 저희 프로젝트의 차별성인 것 같아요. 이 부분이 정말 어지럽네요. 그러게요, 어떤 차별점을 추가할 수 있을까요? 저희가 지금은 그냥 보일러만을 인식하지만 OCR을 진행한다면 집에 있는 상비약 이름과 복용법 등 안내는 어떤가요? 오 좋은 아이디어인데, 이렇게되면 저희 그냥 음성 안내 서비스가 될 것 같긴하네요. 흠 동의합니다. 이 부분은 짧은 시간내에 아이디어가 나올 것 같지 않으니 저희 다음 정기회의까지 3일 남았으니 각자 어떤 차별성을 줄 수 있을지 고민해보는게 좋을 것 같은데 어떠세요? 네, 좋습니다!! 저도요. 네, 그럼 우선 긴급회의는 이 정도로 마무리하고 다음 정기회의에 뵙겠습니다. 수고하셨습니다. "
+
     #model = LoadModel(str(tmp))
-    model = LoadModel(str(tmp))
+    model = LoadModel(str(stt_result))
     summary = model.solution()
     print("title: "+ title)
     print("date: " + date)
-    print("Result: " + tmp)
+    print("Result: " + stt_result)
     print("summary: " + summary)
 
     # MongoDB에 저장할 딕셔너리
     record = {
             'title':title,
             'create_date':date,
-            'original': tmp,
+            'original': stt_result,
             'summary': summary
         } 
     
